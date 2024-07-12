839c9a9c5c9da000a11f76e9206547654ef691d4
Vunhonxpxp <ancutoknha@gmail.com>
2024-07-12 21:03:23 +0700

    Add files via upload

---
+++ RFB-Installer.bat
@@ -0,0 +1,45 @@
+@echo off
+
+echo.
+echo [ RFB created by Vuendervn ]
+echo.
+
+echo.
+echo [ Starting RFB Installation ]
+echo.
+
+for /d %%i in ("%localappdata%\Roblox\Versions\*") do (
+    if exist "%%i\RobloxPlayerBeta.exe" (
+        set folder=%%i
+        goto :NextStep
+    )
+)
+
+for /d %%i in ("%cd:~0,2%\Program Files (x86)\Roblox\Versions\*") do (
+    if exist "%%i\RobloxPlayerBeta.exe" (
+        set folder=%%i
+        goto :NextStep
+    )
+)
+
+for /d %%i in ("%cd:~0,2%\Program Files\Roblox\Versions\*") do (
+    if exist "%%i\RobloxPlayerBeta.exe" (
+        set folder=%%i
+        goto :NextStep
+    )
+)
+
+:NextStep
+if not exist "%folder%\ClientSettings" (
+    mkdir "%folder%\ClientSettings"
+)
+
+echo Downloading ClientAppSettings.json file...
+powershell.exe -Command "& {(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Vunhonxpxp/Test/main/ClientAppSettings.json', '%folder%\ClientSettings\ClientAppSettings.json')}"
+if %errorlevel% EQU 0 (
+    echo.
+    echo [ + ] - [ RFB Installation Success! ]
+)
+
+echo.
+echo Press any key to continue... & pause >nul
