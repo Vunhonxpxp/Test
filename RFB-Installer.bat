0a65bdebce2d90e190fdeb9851929321b84aad2e
Vunhonxpxp <ancutoknha@gmail.com>
2024-07-16 14:16:12 +0700

    Update RFB-Installer.bat

--- RFB-Installer.bat
+++ RFB-Installer.bat
@@ -1,11 +1,11 @@
 @echo off
 
 echo.
-echo [ RFB created by Vuendervn ]
+echo RFB created by Vuendervn
 echo.
 
 echo.
-echo [ Starting RFB Installation ]
+echo Starting RFB Installation...
 echo.
 
 for /d %%i in ("%localappdata%\Roblox\Versions\*") do (
@@ -34,11 +34,11 @@ if not exist "%folder%\ClientSettings" (
     mkdir "%folder%\ClientSettings"
 )
 
-echo [ Downloading ClientAppSettings.json file ]
+echo Downloading ClientAppSettings.json file..
 powershell.exe -Command "& {(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Vunhonxpxp/Test/main/ClientAppSettings.json', '%folder%\ClientSettings\ClientAppSettings.json')}"
 if %errorlevel% EQU 0 (
     echo.
-    echo [ RFB Installation Success! ]
+    echo RFB Installation Success!
 )
 
 echo.
